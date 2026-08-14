ace49ef252fedd8559b0adf6957e4a9fa92f3726
izequiel <izequiel.r.alves@gmail.com>
2026-08-13 23:11:56 -0300

    adiciona e modifica estes exercícios de arrays

--- Ex6_Fatorial.java
+++ Ex6_Fatorial.java
@@ -1,4 +1,30 @@
 package estudos.Izequiel.repeticaoArrays;
 
+/*
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+EX: 5!= 120 (5 X 4 X  3 X 2 X 1)
+ */
+
+import java.util.Scanner;
+
 public class Ex6_Fatorial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Fatorial: ");
+        int fatorial = scanner.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.println(fatorial + "! = ");
+
+        for (int i = fatorial; i >= 1; i--) {
+            multiplicacao = multiplicacao * i;
+
+        }
+        System.out.println(multiplicacao);
+
+
+    }
 }
